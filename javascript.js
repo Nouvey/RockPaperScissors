f7c60f64716bb21b2c536e91ffea02e95549b750
Nouvelle <nouvellebecca@gmail.com>
2023-02-02 04:56:42 +0300

    adding js file and html

--- javascript.js
+++ javascript.js
@@ -1,3 +1,56 @@
-function getComputerChoice(){
+const choice = ["rock", "paper", "scissors"];
+
+
+function getComputerChoice() {
+    const options = choice[Math.floor(Math.random() * choice.length)];
+    return options;
+}
+getComputerChoice();
+
+function checkWinner(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "Tie";
+    }
+    else if (
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")) {
+        return "Player Wins"
+    }
+
+    else{
+        return "Computer Wins"
+    }
 
 }
+
+
+function roundOfPlay(playerSelection, computerSelection) {
+    const result = checkWinner(playerSelection, computerSelection);
+    if (result == "Tie") {
+        return "It's a tie"
+    }
+    else if (result == "Player Wins") {
+        return `Player wins this round, ${playerSelection} beats ${computerSelection}`
+    }
+    else {
+        return `Computer wins, ${computerSelection} beats ${playerSelection}`
+    }
+
+
+}
+
+const playerSelection = "rock"
+const computerSelection = getComputerChoice();
+console.log(roundOfPlay(playerSelection,computerSelection));
+
+function game(){
+    console.log("Hello")
+    for (let i=0; i<5; i++){
+        const playerSelection = "rock"
+        const computerSelection = getComputerChoice();
+        console.log(roundOfPlay(playerSelection,computerSelection))
+    }
+}
+
+game()
